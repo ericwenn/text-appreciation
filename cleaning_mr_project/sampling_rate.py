606beaadbb9b09422a82b4676b8d3c2b506105d0
Stelios_Filippou <sphilip016@gmail.com>
2018-10-17 22:45:21 +0200

    -calculating sampling rate -i did not yet create the aggregated files

---
+++ cleaning_mr_project/sampling_rate.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import re
+
+folder_name="eye-tracking_data/"
+doc_id=["322","2725","3775","3819","4094","4504","4584","4701","5938","6046","6366","6474","7784","9977","10879","11143","11299","13165"]
+mean_rate_dict={}
+#loop through all participants
+for i in range(1,37):
+    #loop through all articles id
+    for j in range(0,len(doc_id)):
+        #create the path to the file
+        if i<10:
+            file_name = 'P0' + str(i) + '_' + str(doc_id[j])
+        else:
+            file_name = 'P' + str(i) + '_'+ str(doc_id[j])
+        path=folder_name+file_name+".txt"
+        file=Path(path)
+        #check if the file exist
+        if file.exists():
+            #read the file
+            with open(path) as f:
+                lines = f.readlines()
+            f.close()
+
+            # calculate sampling rate and ratio
+            period_list = []
+            for k in range(1, len(lines)):
+                x = re.split(',+', lines[k])
+                if k == 1:
+                    first = int(x[0])
+                else:
+                    second = int(x[0])
+                    period_list.append(-1 * (first - second))
+                    first = second
+
+            #calculate mean period for this file
+            mean_period=0
+            for k in range(0,len(period_list)):
+                mean_period=mean_period+period_list[k]
+            mean_period=mean_period/len(period_list)
+            #add it into a dictionary
+            mean_rate_dict.update({file_name : float("{0:.2f}".format((1/(mean_period*(10**-6)))))})
+
+#print all files mean period--
+print(mean_rate_dict)
