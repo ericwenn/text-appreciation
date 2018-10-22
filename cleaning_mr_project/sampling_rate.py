ce0af6ce2eb13dfca82b018f992d63bbfce38b98
Stelios_Filippou <sphilip016@gmail.com>
2018-10-23 00:45:37 +0200

    -some graphs -dif x/ dif y created but not working since we run into a problem

--- cleaning_mr_project/sampling_rate.py
+++ cleaning_mr_project/sampling_rate.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import re
 import matplotlib.pyplot as plt
+import math
 folder_name="eye-tracking_data/"
 doc_id=["322","2725","3775","3819","4094","4504","4584","4701","5938","6046","6366","6474","7784","9977","10879","11143","11299","13165"]
 mean_rate_dict={}
+stand_dev={}
 #loop through all participants
 
 for i in range(1,37):
@@ -40,17 +42,33 @@ for i in range(1,37):
                 mean_period=mean_period+period_list[k]
             mean_period=mean_period/len(period_list)
             #add it into a dictionary
-            mean_rate_dict.update({file_name : float("{0:.2f}".format((1/(mean_period*(10**-6)))))})
+            mean_rate=float("{0:.2f}".format((1/(mean_period*(10**-6)))))
+            mean_rate_dict.update({file_name :mean_rate })
+            var=0
+            for k in range(0, len(period_list)):
+                temp=mean_rate-(1/(period_list[i]*(10**-6)))
+                var = var + (temp**2)
+
+            st=float("{0:.2f}".format(math.sqrt(var/len(period_list))))
+            stand_dev.update({file_name:st})
 
 #print all files mean period--
 all_keys=list(mean_rate_dict.keys())
 values=[]
+values2=[]
 for i in range(0,len(all_keys)):
     values.append(mean_rate_dict.get(all_keys[i]))
-
+    values2.append(stand_dev.get(all_keys[i]))
 plt.plot(all_keys ,values, 'ro',markersize=4 )
 plt.ylabel('Mean Samplng Rate')
 plt.xlabel('File')
 plt.show()
+
+
+plt.plot(all_keys ,values2, 'ro',markersize=4 )
+plt.ylabel('Standard Deviation')
+plt.xlabel('File')
+plt.show()
+
 #exit()
 #print(mean_rate_dict)
