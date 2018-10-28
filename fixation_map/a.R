7b62a8460d13c4186b469a91c539be59602cd2aa
Stelios_Filippou <sphilip016@gmail.com>
2018-10-28 10:04:32 +0100

    no message

--- fixation_map/a.R
+++ fixation_map/a.R
@@ -1,6 +1,6 @@
 library("emov")
 library("ggplot2")
-file=P01_13165
+file=P04_13165
 file$x = filter(file$L.POR.X..px., rep(1/15, 15))
 file$y = filter(file$L.POR.Y..px., rep(1/15, 15))
 fixations_2 = emov.idt(file$Time, file$L.POR.X..px., file$L.POR.Y..px., 100,4)
